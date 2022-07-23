7ae44de356455d29e2340e4ff8003ff1c1bc7de4
Kunj <shahkunj42@gmail.com>
2022-07-23 16:27:48 -0400

    commit tree e7d72df2dcb99762a0b3b2b967ae81626df98e7a

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    self.actors.pluck(:first_name, :last_name).join(' ')
+  end
 end
